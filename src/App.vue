2adf53b44b7228cc227fb3cd086e507dd9db8c5f
Emily Carlsen <emilytcarlsen@gmail.com>
2020-07-11 00:06:16 -0400

    Add v1 design with social and project links

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,29 @@
       <v-container fill-height>
         <v-row align="center" justify="center">
           <v-col class="text-center">
+            <v-img class="elevation-0 mx-auto" width="256" src="@/assets/emily.png"></v-img>
+            <v-divider class="mx-auto" style="width: 600px;"></v-divider>
             <h1 class="name">Emily Carlsen</h1>
-            <h4 class="subtitle">Coming soon.</h4>
+            <v-row>
+              <v-col class="text-center">
+                <v-btn target="_blank" href="https://twitter.com/emilyplusplus" icon><v-icon>mdi-twitter</v-icon></v-btn>
+                <v-btn target="_blank" href="https://www.instagram.com/emilyplusplus/" icon><v-icon>mdi-instagram</v-icon></v-btn>
+                <v-btn target="_blank" href="https://github.com/emilyplusplus" icon><v-icon>mdi-github</v-icon></v-btn>
+                <v-btn target="_blank" href="https://www.youtube.com/channel/UChoXabQMSsYA8JRbZ_St65Q" icon><v-icon>mdi-youtube</v-icon></v-btn>
+                <v-btn target="_blank" href="https://www.linkedin.com/in/emilyplusplus/" icon><v-icon>mdi-linkedin</v-icon></v-btn>
+                <v-btn target="_blank" href="https://repl.it/@EmilyCarlsen" icon><v-icon>mdi-code-braces-box</v-icon></v-btn>
+
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col class="text-center">
+                <b>Projects</b><br /><br />
+                <a target="_blank" href="https://jsoninspect.com">jsoninspect.com</a><br />
+                <a target="_blank" href="https://emilydiy.com">emiydiy.com</a><br />
+                ---<br />
+                <a target="_blank" href="https://emilyplusplus.wordpress.com">emilyplusplus.wordpress.com</a>
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
       </v-container>
@@ -31,12 +52,10 @@ export default {
 
 <style lang="scss" scoped>
 .v-main {
-  background: #b3e5fc;
+  background: #f5f5f5;
 }
 .name {
-  font-size: 4em;
-}
-.subtitle {
-  font-size: 1.4em;
+  font-size: 3em;
+  color: #212121;
 }
 </style>
