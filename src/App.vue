fe3513266cf45f9265b7cb45b35aef74140df9e2
Emily Carlsen <emilytcarlsen@gmail.com>
2020-10-24 16:02:02 -0400

    Update links

--- src/App.vue
+++ src/App.vue
@@ -12,19 +12,17 @@
                 <v-btn target="_blank" href="https://twitter.com/emilyplusplus" icon><v-icon>mdi-twitter</v-icon></v-btn>
                 <v-btn target="_blank" href="https://www.instagram.com/emilyplusplus/" icon><v-icon>mdi-instagram</v-icon></v-btn>
                 <v-btn target="_blank" href="https://github.com/emilyplusplus" icon><v-icon>mdi-github</v-icon></v-btn>
-                <v-btn target="_blank" href="https://www.youtube.com/channel/UChoXabQMSsYA8JRbZ_St65Q" icon><v-icon>mdi-youtube</v-icon></v-btn>
+                <v-btn target="_blank" href="https://www.youtube.com/channel/UCjxTwAtQFeFRZot42efMIew" icon><v-icon>mdi-youtube</v-icon></v-btn>
+                <v-btn target="_blank" href="https://www.twitch.tv/EmilyCarlsen" icon><v-icon>mdi-twitch</v-icon></v-btn>
                 <v-btn target="_blank" href="https://www.linkedin.com/in/emilyplusplus/" icon><v-icon>mdi-linkedin</v-icon></v-btn>
                 <v-btn target="_blank" href="https://repl.it/@EmilyCarlsen" icon><v-icon>mdi-code-braces-box</v-icon></v-btn>
-
+                <v-btn target="_blank" href="emilyplusplus.wordpress.com" icon><v-icon>mdi-wordpress</v-icon></v-btn>
               </v-col>
             </v-row>
             <v-row>
               <v-col class="text-center">
-                <b>Projects</b><br /><br />
+                <b>Software Projects</b><br /><br />
                 <a target="_blank" href="https://jsoninspect.com">jsoninspect.com</a><br />
-                <a target="_blank" href="https://emilydiy.com">emiydiy.com</a><br />
-                ---<br />
-                <a target="_blank" href="https://emilyplusplus.wordpress.com">emilyplusplus.wordpress.com</a>
               </v-col>
             </v-row>
           </v-col>
